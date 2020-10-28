bbcb14661b3eb1d40c220e78a79a114bb0c10002
alexanderytaylor <alexanderytaylor@gmail.com>
2020-10-27 18:36:26 -0700

    tmdb_repository finished and working for now!

--- packages/tmdb_repository/example/main.dart
+++ packages/tmdb_repository/example/main.dart
@@ -1,1 +1,22 @@
+import 'dart:io';
+import 'package:tmdb_repository/tmdb_repository.dart';
 
+void main() async {
+  final String tmdbAPIKey = "59a3a927d44125cb8b5788a5013b294a";
+
+  final tmdbRepository = TMDBRepository(tmdbAPIKey: tmdbAPIKey);
+  final String query = "The Hangover";
+
+  try {
+    final results = await tmdbRepository.search(
+      query: query,
+    );
+    for (var result = 0; result < results.results.length; result++) {
+      print(results.results[result].title);
+    }
+  } on Exception catch (e) {
+    print(e);
+  }
+
+  exit(0);
+}
