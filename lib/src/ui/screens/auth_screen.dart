d655f74887a3b7c7828eac75d2bc67b9618eab74
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2021-01-20 17:04:15 +0500

    Login screen ready

--- lib/src/ui/screens/auth_screen.dart
+++ lib/src/ui/screens/auth_screen.dart
@@ -7,38 +7,85 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          // Logo
-          Text(
-            'Butler',
-            style: kTitleTextStyle,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Login'),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-              textStyle: MaterialStateProperty.all(
-                TextStyle(
-                  color: Colors.white,
-                ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Logo
+              Spacer(),
+              CircleAvatar(
+                child: Text('T'),
+                radius: 46.0,
               ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Login'),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              textStyle: MaterialStateProperty.all(
-                TextStyle(color: Colors.black),
+              SizedBox(
+                height: 24.0,
               ),
-            ),
+              Text(
+                'Butler',
+                style: kTitleTextStyle,
+                textAlign: TextAlign.center,
+              ),
+              Spacer(),
+              RoundedRectangleButton(
+                onPressed: () {},
+                buttonText: 'Login',
+                buttonColour: Colors.black,
+                buttonTextColour: Colors.white,
+              ),
+
+              RoundedRectangleButton(
+                onPressed: () {},
+                buttonText: 'Sign up',
+              ),
+            ],
           ),
-        ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedRectangleButton extends StatelessWidget {
+  const RoundedRectangleButton({
+    Key key,
+    @required this.onPressed,
+    @required this.buttonText,
+    this.buttonTextColour,
+    this.buttonColour = Colors.white,
+    this.buttonBorderRadius = 44.0,
+  }) : super(key: key);
+
+  final String buttonText;
+  final Color buttonTextColour;
+  final Color buttonColour;
+  final double buttonBorderRadius;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: this.onPressed,
+      child: Text(
+        this.buttonText,
+        style: kButtonTextStyle.copyWith(
+          color: this.buttonTextColour,
+        ),
+      ),
+      style: ButtonStyle(
+        side: MaterialStateProperty.all<BorderSide>(
+          BorderSide(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: MaterialStateProperty.all<Color>(this.buttonColour),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(this.buttonBorderRadius),
+          ),
+        ),
       ),
     );
   }
--- lib/src/utilities/constants.dart
+++ lib/src/utilities/constants.dart
@@ -3,3 +3,8 @@ import 'package:flutter/material.dart';
 const kTitleTextStyle = TextStyle(
   fontSize: 20.0,
 );
+
+const kButtonTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 16.0,
+);
