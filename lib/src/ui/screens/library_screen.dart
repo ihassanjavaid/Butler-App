5dec59f9cd82bbde199c562aac2e85f65c4e15e3
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2021-01-22 22:39:12 +0500

    Bloc integrated

--- lib/src/ui/screens/library_screen.dart
+++ lib/src/ui/screens/library_screen.dart
@@ -1,45 +1,73 @@
+import 'package:butler_app/src/bloc/library_bloc.dart';
 import 'package:butler_app/src/bloc/menu_bloc.dart';
-import 'package:butler_app/src/resources/menu_repository.dart';
+import 'package:butler_app/src/resources/services/movie_service.dart';
 import 'package:butler_app/src/resources/utilities/constants.dart';
-import 'package:butler_app/src/resources/utilities/searchtype_enum.dart';
 import 'package:butler_app/src/ui/widgets/search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LibraryScreen extends StatefulWidget {
+class LibraryScreen extends StatelessWidget {
   static const String id = 'library_screen';
-  final SearchType searchType;
 
-  LibraryScreen([this.searchType]);
-
-  @override
-  _LibraryScreenState createState() => _LibraryScreenState();
-}
-
-class _LibraryScreenState extends State<LibraryScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return SafeArea(
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          vertical: 18.0,
-          horizontal: 12.0,
-        ),
-        height: size.height,
-        width: size.width,
-        color: kBackgroundColor,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            _getNavLinks(),
-            SizedBox(
-              height: size.height / 24 - 10,
-            ),
-            Material(
-              child: Search(),
-            ),
-          ],
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 18.0,
+            horizontal: 12.0,
+          ),
+          height: size.height,
+          width: size.width,
+          color: kBackgroundColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              _getNavLinks(),
+              SizedBox(
+                height: size.height / 24 - 10,
+              ),
+              Material(
+                child: Search(
+                  onChanged: (val) {
+                    BlocProvider.of<LibraryBloc>(context)
+                        .add(SearchEntryEvent(val));
+                  },
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  BlocProvider.of<LibraryBloc>(context).add(SearchEvent());
+                },
+                child: Text('Search'),
+              ),
+              Expanded(
+                child:
+                    BlocBuilder<LibraryBloc, LibraryState>(builder: (_, state) {
+                  List<Results> result = [];
+                  if (state is SearchingLibrary) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (state is ResultState) {
+                    result = state.movieSearchResult.results;
+                    return ListView.builder(
+                      itemBuilder: (_, index) {
+                        return Text(
+                          result[index].title,
+                          style: TextStyle(color: Colors.white),
+                        );
+                      },
+                      itemCount: result.length,
+                    );
+                  }
+                  return Container();
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -54,7 +82,9 @@ class _LibraryScreenState extends State<LibraryScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Icon(
               Icons.movie,
-              color: kGreyColor,
+              color: state is MovieState
+                  ? kSelectedIconColour
+                  : kDefaultIconColour,
               size: 30,
             ),
           ),
@@ -62,7 +92,8 @@ class _LibraryScreenState extends State<LibraryScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Icon(
               Icons.menu_book,
-              color: kGreyColor,
+              color:
+                  state is BookState ? kSelectedIconColour : kDefaultIconColour,
               size: 30,
             ),
           ),
@@ -70,7 +101,9 @@ class _LibraryScreenState extends State<LibraryScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Icon(
               Icons.queue_music,
-              color: kGreyColor,
+              color: state is MusicState
+                  ? kSelectedIconColour
+                  : kDefaultIconColour,
               size: 30,
             ),
           ),
@@ -78,24 +111,33 @@ class _LibraryScreenState extends State<LibraryScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Icon(
               Icons.videogame_asset,
-              color: kGreyColor,
+              color:
+                  state is GameState ? kSelectedIconColour : kDefaultIconColour,
               size: 30,
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Icon(
-              Icons.movie,
-              color: kGreyColor,
+              Icons.tv,
+              color: state is TVShowState
+                  ? kSelectedIconColour
+                  : kDefaultIconColour,
+              size: 30,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Icon(
+              Icons.mic,
+              color: state is PodcastState
+                  ? kSelectedIconColour
+                  : kDefaultIconColour,
               size: 30,
             ),
           ),
         ],
       );
     });
-  }
-
-  Color _getIconColour(MenuState state, IconType iconType) {
-    if (state is MovieState) {}
   }
 }
